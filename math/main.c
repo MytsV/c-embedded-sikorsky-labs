2462c07d8e0bed091faabeeb2cde7abf259a53b4
MytsV <mytsvictoria@gmail.com>
2023-10-09 00:22:21 +0200

    Fix formatting according to Google style

--- math/main.c
+++ math/main.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 void read(char *name, double *var) {
-    printf(name);
-    while(scanf("%lf", var) != 1) {
-        printf("Wrong input! Try again. %s", name);
-        scanf("%*s");
-    }
+  printf(name);
+  while (scanf("%lf", var) != 1) {
+    printf("Wrong input! Try again. %s", name);
+    scanf("%*s");
+  }
 }
 
 int main() {
-    double a;
-    double b;
-    read("a = ", &a);
-    read("b = ", &b);
-    printf("z1 = %lf\nz2 = %lf\n", calc_first(a, b), calc_second(b));
-    return 0;
+  double a;
+  double b;
+  read("a = ", &a);
+  read("b = ", &b);
+  printf("z1 = %lf\nz2 = %lf\n", calc_first(a, b), calc_second(b));
+  return 0;
 }
--- math/src/solution.c
+++ math/src/solution.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 double calc_first(double a, double b) {
-    return (sin(a) + cos(2 * b - a))/(cos(a) - sin(2 * b - a));
+  return (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
 }
 
-double calc_second(double b) {
-    return (1 + sin(2 * b))/cos(2 * b);
-}
+double calc_second(double b) { return (1 + sin(2 * b)) / cos(2 * b); }
